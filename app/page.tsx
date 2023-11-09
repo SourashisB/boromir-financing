86ea1a184322c170e99ddf0a6b514b269b354196
Sourashis.Bhowmik <sourashis.bhowmik@fdmgroup.com>
2023-11-09 17:22:45 +0800

    Landing Page Bugfix

--- app/page.tsx
+++ app/page.tsx
@@ -3,17 +3,30 @@
 import React from "react";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
-import { Box, Container } from "@mui/material";
+import { Box, Container, Button } from "@mui/material";
+import insPhoto from '/public/insurance-photo-1.jpg';
+
 export default function Home() {
+
+
   return (
     <React.Fragment>
-      <Header/>
-        <Container maxWidth="lg" >
-        <Box sx={{ bgcolor: '#cfe8fc', height: '100%' }}>
-            
-          </Box>
-        </Container>
-      <Footer/>
-    </React.Fragment>
+      <Header />
+      <Container maxWidth="lg" >
+        <div style={{
+          backgroundImage: 'url(@/public/insurance-photo-1.jpg)',
+          height:'350px',
+          width:'550px',
+          backgroundSize: 'contain',
+          backgroundRepeat: 'no-repeat'
+        }}/>
+        <Box sx={{ backgroundImage: `url(${insPhoto})`, height:'auto', width:'auto', display:'block' }}>
+          <Button variant="contained" className="test-button">
+            Test button
+          </Button>
+        </Box>
+      </Container>
+      <Footer />
+    </React.Fragment >
   )
 }
